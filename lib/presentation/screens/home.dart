fadf101330c95db9f0d7af67a1e43a28526b0996
besufikad17 <besufikadtilahun1994@gmail.com>
2023-10-09 16:08:17 -0400

    sliding menu widget added

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -1,9 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cleanarchdemo/data/repository/local_storage_repository_impl.dart';
 import 'package:cleanarchdemo/locator.dart';
-import 'package:cleanarchdemo/presentation/components/app_bar.dart';
-import 'package:cleanarchdemo/utils/resources/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 
 @RoutePage()
 class HomePage extends StatelessWidget {
@@ -17,32 +16,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var token = getToken();
-
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(100),
-        child: MyAppBar(
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.person),
-              color: getColorFromHex("#000000"),
-              onPressed: () {}
-            )
-          ],
+      body: SliderDrawer(
+        slider: Container(),
+        appBar: const SliderAppBar(
+          title: Text("Home"),
+          isTitleCenter: true,
         ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 50,),
-            FutureBuilder(
-              future: getToken(), 
-              builder: ((BuildContext context, snapshot) {
-                return Text(snapshot.data ?? "no token!!");
-              })
-            )
-          ],
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50,),
+              FutureBuilder(
+                future: getToken(), 
+                builder: ((BuildContext context, snapshot) {
+                  return Text(snapshot.data ?? "no token!!");
+                })
+              )
+            ],
+          ),
         ),
       ),
     );
